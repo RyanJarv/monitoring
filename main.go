ea91ccde100d4716fde51ee020c43d4a3a612cc0
Ryan Gerstenkorn <ryan_gerstenkorn@fastmail.fm>
2019-05-21 19:05:54 -0700

    test

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"io/ioutil"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -10,15 +8,17 @@ import (
 // Handler is executed by AWS Lambda in the main function. Once the request
 // is processed, it returns an Amazon API Gateway response object to AWS Lambda
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-	index, err := ioutil.ReadFile("public/index.html")
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
+	// request.RequestContext
+	// switch request.HTTPMethod	{
+	// case "GET"
+	// 	if (request.Path == "")
+	// case "POST"
+	// case "DELETE"
+	// }
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       string(index),
+		Body:       string(request.Body),
 		Headers: map[string]string{
 			"Content-Type": "text/html",
 		},
